ee5c4df2f17833ac27158c2f4ca4349202e26be6
Mekan <mekanh@gmail.com>
2019-02-01 20:11:28 -0600

    Reorganize annotations and close driver

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -4,7 +4,9 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
@@ -12,12 +14,14 @@ import java.util.concurrent.TimeUnit;
 public class LoginTests {
     WebDriver driver;
 
-    @BeforeClass
+    @BeforeMethod
     public void setUp() {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
+
+
 
     @Test
     public void loginTest1() {
@@ -26,4 +30,11 @@ public class LoginTests {
         driver.findElement(By.id("ctl00_MainContent_password")).sendKeys("test" + Keys.ENTER);
         Assert.assertEquals(driver.getTitle(), "Web Orders");
     }
+
+    @AfterMethod
+    public void cleanUp(){
+        driver.close();
+    }
+
+
 }
