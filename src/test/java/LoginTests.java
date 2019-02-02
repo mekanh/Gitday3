2e4d66f6dcb6d5805f31c25d1ad73617d8860ab0
Mekan <mekanh@gmail.com>
2019-02-01 21:17:35 -0600

    added login test2

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -38,6 +38,13 @@ public class LoginTests {
         Assert.assertEquals(driver.getTitle(), "Web Orders Login");
     }
 
+    @Test
+    public void loginTest2() {
+        driver.get("http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx");
+        driver.findElement(By.id("ctl00_MainContent_username")).sendKeys("Tester");
+        driver.findElement(By.id("ctl00_MainContent_password")).sendKeys("test" + Keys.ENTER);
+        Assert.assertEquals(driver.getTitle(), "Web Orders");
+    }
     @AfterMethod
     public void cleanUp(){
         driver.close();
